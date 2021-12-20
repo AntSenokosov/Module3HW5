5b5c57869de87fcad6e0384f338ac139e91c8600
AntSenokosov <a.y.senokosov@gmail.com>
2021-12-20 20:05:11 +0200

    Add realization in methods ReadFile1Async and ReadFile2Async

--- Module3HW5/Module3HW5/Program.cs
+++ Module3HW5/Module3HW5/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Module3HW5
 {
@@ -8,12 +10,28 @@ namespace Module3HW5
         {
         }
 
-        static void ReadFile1Async()
+        static async Task<string> ReadFile1Async()
         {
+            string result = string.Empty;
+
+            using (StreamReader reader = new StreamReader("file1.txt"))
+            {
+                result = await reader.ReadToEndAsync();
+            }
+
+            return result;
         }
 
-        static void ReadFile2Async()
+        static async Task<string> ReadFile2Async()
         {
+            string result = string.Empty;
+
+            using (StreamReader reader = new StreamReader("file2.txt"))
+            {
+                result = await reader.ReadToEndAsync();
+            }
+
+            return result;
         }
     }
 }
